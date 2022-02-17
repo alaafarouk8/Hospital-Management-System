da6d3800ec38caa3871a3654de82cdce47d8ad60
ALaa Farouk <alaafar19@gmail.com>
2022-02-17 20:59:43 +0200

    last lab

--- iti/hms/__manifest__.py
+++ iti/hms/__manifest__.py
@@ -8,11 +8,15 @@
     'depends': ['base', 'crm'],
     # data files always loaded at installation
     'data': [
+        'security/security.xml',
+        'security/ir.model.access.csv',
         'views/hms_patient_view.xml',
         'views/hms_departments_view.xml',
         'views/hms_doctor_view.xml',
         'views/hms_log_history.xml',
-        'views/crm_inherit_view.xml'
+        'views/crm_inherit_view.xml',
+        'reports/report.xml',
+        'reports/template.xml',
     ],
 
 }
